6a87aef85f577d169987560d49f5da80e05add16
gmillanz <155916784+gmillanz@users.noreply.github.com>
2024-02-20 15:46:43 -0300

    Add files via upload

---
+++ projeto.rb
@@ -0,0 +1,7 @@
+print "digite o seu nome: "
+nome = gets.chomp
+print "digite o seu sobrenome: "
+sobrenome = gets.chomp
+print "sua idade: "
+idade = gets.chomp 
+puts "Bem vindo #{nome} #{sobrenome}, você tem #{idade} anos"
